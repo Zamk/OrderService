b9a25ebb82a8ae8888210ee0f249b23eb6de646e
Nikita <nikita@morion.tech>
2022-07-31 10:06:39 +0300

    Добавьте файлы проекта.

---
+++ OrderService.iikoTransportApi/TokenStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.iikoTransportApi
+{
+    public class TokenStorage
+    {
+        private string _token;
+        private DateTime _expires;
+
+        public string Token 
+        { 
+            get
+            {
+                return _token;
+            } 
+            private set
+            {
+                _token = value;
+            }
+        }
+    }
+}
