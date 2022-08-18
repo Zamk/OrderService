210e50f1f97264ccb01a58a0dcd2350173d03e36
Никита Шляков <nikita@morion.tech>
2022-08-18 09:29:29 +0300

    Some improvements for TokenStorage

--- OrderService.iikoTransportApi/TokenStorage.cs
+++ OrderService.iikoTransportApi/TokenStorage.cs
@@ -5,25 +5,35 @@
         private string _token;
         private DateTime _expires;
         private string _apiKey;
+        private int _tokenExpirationTime;
 
-        public TokenStorage(string apiLogin)
+        /// <summary>
+        /// Конструктор класса для хранения, получения и обновления токена.
+        /// </summary>
+        /// <param name="apiLogin">iikoTransport ApiLogin</param>
+        /// <param name="tokenExpirationTime">Срок жизни токена, в минутах</param>
+        public TokenStorage(string apiLogin, int tokenExpirationTime = 20)
         {
-            _expires = DateTime.Now + TimeSpan.FromMinutes(20);
-            _token = null;
+            _expires = DateTime.Now;
+            _token = String.Empty;
             _apiKey = apiLogin;
+            _tokenExpirationTime = tokenExpirationTime;
         }
-
-        public string Token { get { return _token; } }
 
         public string ApiKey { get { return _apiKey; } }
 
+        /// <summary>
+        /// Получает актуальный токен iikoTransport API
+        /// </summary>
+        /// <param name="client">Клиент, для для которого получем токен</param>
+        /// <returns>String содержащий токен авторизации</returns>
         public async Task<string> GetToken(iikoTransportClient client)
         {
-            if (_token == null || DateTime.Now >= _expires)
+            if (_token == null || _token == String.Empty || DateTime.Now >= _expires)
             {
                 var token = await client.GetAccessTokenAsync();
                 _token = token.Token;
-                _expires = DateTime.Now + TimeSpan.FromMinutes(20);
+                _expires = DateTime.Now + TimeSpan.FromMinutes(_tokenExpirationTime);
             }
 
             return _token;
