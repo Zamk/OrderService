3273219f46cde454e4ea5af644d09edade30a8fa
Никита Шляков <nikita@morion.tech>
2022-08-20 16:43:54 +0300

    Add product component

--- OrderService.Frontend/src/app/app.module.ts
+++ OrderService.Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
